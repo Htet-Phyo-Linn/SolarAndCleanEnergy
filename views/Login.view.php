5d5b4debd83769faabfb88d9984671b531cf7dd8
Htet Phyo Linn <109867841+Htet-Phyo-Linn@users.noreply.github.com>
2023-02-20 10:47:00 +0630

    check lovin

--- views/Login.view.php
+++ views/Login.view.php
@@ -8,8 +8,8 @@
         <p> name or password wrong. </p>
     <?php
         App::bind("status", "success");
-        if(App::bind("status", "success") {
-            
+        if(App::bind("status") == "success") {
+            redirect("home");
         }
         }
     ?>
